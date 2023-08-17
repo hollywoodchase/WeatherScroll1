34deafe36a935a0c13c43db3b8aa97edf83c90bb
Danny Calise <danny.calise@us.kbr.com>
2023-08-17 07:18:21 -0400

    successful mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,23 @@
-const express = require('express');
-const MongoClient = require('mongodb').MongoClient;
-const bodyParser = require('body-parser');
-const app = express();
-const port = 8000;
- app.use(bodyParser.urlencoded({extended: true}));
-  MongoClient.connect( "mongodb+srv://hollywoodchase:bigNsmall517!@cluster0.xnreyj9.mongodb.net/?retryWrites=true&w=majority",{ useNewUrlParser: true }, (err, database) => 
-  {
-     if (err) return console.log(err);
-     require('./app/routes')(app, database);
-      app.listen(port, () => {
-           console.log("We are live on " +port);
-  });
- });
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://hollywoodchase:bigNsmall517!@cluster0.xnreyj9.mongodb.net/?retryWrites=true&w=majority";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
